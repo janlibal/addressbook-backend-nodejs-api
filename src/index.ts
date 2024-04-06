fa1467cf03753b15166669c456a9e31573328040
Jan Libal <libalj@seznam.cz>
2024-04-06 17:55:00 +0200

    Update server launch

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,46 @@
-import logger from "./utils/logger"
 import config from './config'
+import { close, connect } from './database'
+import logger from './utils/logger'
 import createServer from './utils/server'
 
 export async function server(){
 
   let server: any
+  let db: any
   const srvPort = config.server.port
+  const dbPort = config.database.connection
 
   try {
+
+    db = await connect()
+    logger.info(`Database connected on ${dbPort}. `)
 
     server = createServer
     logger.info(`Server is listening on ${srvPort}. `)
 
-  } catch (err:any) {
+  } catch (err:unknown) {
 
     process.exitCode = 1
     logger.fatal('FATAL ERROR WHILE STARTING SERVER!')
 
   } finally {
+
+    if (!db){
+      logger.fatal("Closing database...")
+      await close()
+      logger.fatal("Database closed")
+    }
+
     if (!server){
-      logger.debug("Closing server...")
-      await server.close()
-      logger.debug("Server closed")
+      logger.fatal("Closing server...")
+      process.on('Unhandled rejection', (err:unknown, promise) => {
+        server.close(() => process.exit(1))
+      })
+      logger.fatal('Server closed')
     }
 
   }
+
 }
 
 server()
