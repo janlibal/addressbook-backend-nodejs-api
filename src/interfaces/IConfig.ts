4a0380fb8d004bede679afc806a7ca4efda890ad
Jan Libal <libalj@seznam.cz>
2024-04-06 11:01:23 +0200

    Update env paths in files

--- src/interfaces/IConfig.ts
+++ src/interfaces/IConfig.ts
@@ -1,9 +1,41 @@
 export interface IConfig {
     appName: string
     nodeVer: string
+        server: {
+            logPath: string,
+            port: string,
+            environment: string,
+            bodyParser: {
+                patchKoa: boolean,
+                urlencoded: boolean,
+                text: boolean,
+                json: boolean,
+                multipart: boolean,
+              },
+            cors: {
+                origin: string,
+                exposeHeaders: []
+                allowedHeaders: []
+                allowedMethods: []
+            }
+        }
+        database: {
+            connection: string
+        }
+    debugLogging: boolean
     pinoMinLevel: string
     pinoEnabled: boolean
-    logPath: string
-    environment: string
-    port: string
+        auth: {
+            secret: string
+            saltRounds: string
+            createOptions: {
+                expiresIn: any
+                algorithm: any
+                issuer: string
+            }
+        }
 }
+
+
+
+
