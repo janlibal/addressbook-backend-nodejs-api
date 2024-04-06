a0f0a6bde927c16f97f2ed591c48c8f4492fc1b8
Jan Libal <libalj@seznam.cz>
2024-04-06 17:48:18 +0200

    Add user model

---
+++ src/database/models/userModel.ts
@@ -0,0 +1,36 @@
+import Base from './base'
+
+class User extends Base{
+    static get tableName() {
+        return 'users'
+    }
+
+    static get jsonSchema() {
+        return {
+            type: 'object',
+            required: ['name', 'email', 'password'],
+            
+            properties: {
+                email: { 
+                    type: 'string', 
+                },
+                name: { 
+                    type: 'string',  
+                },
+                password: { 
+                    type: 'string', 
+                },
+            },
+        }
+    }
+
+    $formatJson(json:any) {
+        json = super.$formatJson(json)
+        delete json.id
+        delete json.password
+        delete json.name
+        return json
+      }
+}
+
+export default User
