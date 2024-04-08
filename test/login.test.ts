3f3ce17c42d779e04e0f9cde7b5b0bb3c47d056b
Jan Libal <libalj@seznam.cz>
2024-04-08 09:26:24 +0200

    Add signIn() test

--- test/login.test.ts
+++ test/login.test.ts
@@ -12,19 +12,29 @@ afterAll(async () => {
 describe('POST /api/v1/login', () => {
 
     beforeEach(async() => {
-        return await knex.migrate.rollback()
-        .then(async () => {return await knex.migrate.latest()})
+      return await knex.migrate.rollback()
+      .then(async () => {return await knex.migrate.latest()})
     })
   
     afterEach(async () => {
       return await knex.migrate.rollback()
     })
   
-    it('Should return 200 to make sure the route works', async () => {
+    it('SignIn test', async () => {
       const request = supertest(server)
- 
+      const userData = {
+        email: 'joe@joedoe.com', 
+        password: 'Password123!'
+      }
       const res = await request
       .post(`/api/v1/login`)
+      .send(userData)
+      .expect('Content-Type', /json/)
       .expect(200)
+      const info = res.body
+      const status = res.status
+      expect(status).toBe(200)
+      expect(info.email).toMatch(/^\S+@\S+\.\S+$/)
+      expect(info.token).toMatch(/^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/)
     })
 })
