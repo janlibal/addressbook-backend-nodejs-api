48258822ee9865bf191eafa4efc3d9eab7b8891f
Jan Libal <libalj@seznam.cz>
2024-04-08 09:36:13 +0200

    Update userSchema with signIn properties

--- src/validations/schemas/userSchema.ts
+++ src/validations/schemas/userSchema.ts
@@ -1,11 +1,20 @@
-  const signUp = {
-    type: 'Object',
-    required: true,
-    properties: {
-      name: { type: 'string', required: true, pattern: '^[A-Za-z. -]+$', maxLength: 80 },
-      email: { type: 'string', required: true, format: 'email', maxLength: 80 },
-      password: { type: 'string', required: true, minLength: 8, maxLength: 80 },
-    },
-  }
+const signIn = {
+  type: 'Object',
+  required: true,
+  properties: {
+    email: { type: 'string', required: true, format: 'email', maxLength: 80 },
+    password: { type: 'string', required: true, minLength: 8, maxLength: 80 },
+  },
+}
+
+const signUp = {
+  type: 'Object',
+  required: true,
+  properties: {
+    name: { type: 'string', required: true, pattern: '^[A-Za-z. -]+$', maxLength: 80 },
+    email: { type: 'string', required: true, format: 'email', maxLength: 80 },
+    password: { type: 'string', required: true, minLength: 8, maxLength: 80 },
+  },
+}
   
-export default { signUp }
+export default { signUp, signIn }
