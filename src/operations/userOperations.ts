fa54f3c22badb5d38f660b448c968202bffe6ae6
Jan Libal <libalj@seznam.cz>
2024-04-08 10:22:42 +0200

    Update create() with generateAccessToken()

--- src/operations/userOperations.ts
+++ src/operations/userOperations.ts
@@ -58,7 +58,7 @@ async function create(input: IUser) {
     let createdUser: any
     createdUser = await userRepository.saveUser(data)
 
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZDU0M2E0YS05ZTkwLTQ1MjAtYTljNS1mMTE2YjgyMTgxOTIiLCJpYXQiOjE3MDk4MTk1MjIsImV4cCI6MTcwOTgyOTUyMiwiaXNzIjoiQ09NLkpBTkxJQkFMLmRldmVsb3BtZW50In0.12jmcKAa20PgC_l4DMYRGBdO8lzbWL1cj2xTvp7lhOk'
+    const token = await crypto.generateAccessToken(createdUser.id)
     
     const fakeUser = {
         email: 'joe.doe@joedoe.com',
