b7a9a6754cbaf1d90be5bcd501277cfdd21c22cb
Jan Libal <libalj@seznam.cz>
2024-04-08 15:22:28 +0200

    Update address.test data validation

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,6 +2,28 @@ import userRepository from "../repositories/userRepository"
 import crypto from "./crypto"
 import { randLastName, randFirstName, randPhoneNumber, randStreetAddress, randEmail } from '@ngneat/falso'
 
+export async function createDummyAndAuthorize() {
+  
+  const pwd = 'Password123!'
+  const name = 'Joe Doe'
+  const email = 'joe.doe@joedoe.com'
+ 
+  const dummyUser = {
+    name: name,
+    email: email,
+    password: await crypto.hashPassword(pwd),
+  }
+
+  let createdUser: any
+
+  createdUser = await userRepository.saveUser(dummyUser)
+
+  createdUser.accessToken = await crypto.generateAccessToken(createdUser.id)
+
+  return createdUser
+    
+}
+
 export async function createDummy() {
     
   const firstName = randFirstName()
