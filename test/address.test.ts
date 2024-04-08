7d632901fa55cd0709c010d0abbfb39c6a86c26a
Jan Libal <libalj@seznam.cz>
2024-04-08 14:53:43 +0200

    Add address() test

--- test/address.test.ts
+++ test/address.test.ts
@@ -8,10 +8,16 @@ afterAll(async () => {
 })
 
 describe('POST /api/v1/address', () => {
-      it('Should return to make sure the route works', async () => {
+    it('Address test', async () => {
         const request = supertest(server)
         const res = await request
         .post(`/api/v1/address`)
         .expect(200)
-    })
+        const info = res.body
+        const status = res.status
+        expect(info._writeTime).toBeInstanceOf(Object)
+        const data = info._writeTime
+        expect(data._seconds).toBeGreaterThan(10)
+        expect(data._nanoseconds).toBeGreaterThan(10)
+  })
 })
