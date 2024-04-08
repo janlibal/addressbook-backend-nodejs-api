0134ca0114d479829948e16cccab7889f97437da
Jan Libal <libalj@seznam.cz>
2024-04-08 14:58:52 +0200

    Update authentication() with authorization header check

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,7 +1,13 @@
 import { Next } from "koa"
 import { IContext } from "../interfaces/IContext"
+import logger from "../utils/logger"
 
 export async function authenticate(ctx:IContext, next:Next) {
+
+    if (!ctx.header.authorization) {
+        logger.info('No authorization defined')
+        throw new Error('No authorization defined')
+    }
 
     return next()
 
